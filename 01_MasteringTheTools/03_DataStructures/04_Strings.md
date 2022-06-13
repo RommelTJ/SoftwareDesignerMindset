9fe13b4a28060c957308ad74e507c72c86992457
Rommel Rico <me@rommelrico.com>
2022-06-12 17:04:39 -0700

    :sparkles: Principles: Formatted Strings

--- 01_MasteringTheTools/03_DataStructures/04_Strings.md
+++ 01_MasteringTheTools/03_DataStructures/04_Strings.md
@@ -3,4 +3,3 @@
 * Strings in python are surrounded by quotation marks.
   * String values are immutable, but their references can be reassigned.
   * You can access strings by index.
-  * Since Python 3.6 you can use f-strings to format strings.
--- 01_MasteringTheTools/03_DataStructures/04_Strings_example.py
+++ 01_MasteringTheTools/03_DataStructures/04_Strings_example.py
@@ -10,12 +10,6 @@ def main():
     print("this" in my_str3)  # True
     print(my_str3[1:7])  # ello t
     print(my_str3[-3])  # i
-    val = 3
-    formatted_str = f"The value is {val}"
-    print(formatted_str)  # The value is 3
-    amount: int = 30000
-    formatted_str2 = f"The amount is ${amount / 100:.2f}"
-    print(formatted_str2)  # The amount is $300.00
 
 
 if __name__ == "__main__":
