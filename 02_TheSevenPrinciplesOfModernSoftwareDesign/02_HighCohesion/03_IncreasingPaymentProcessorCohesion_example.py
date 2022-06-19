13215280c27e54167599c1d1dd8bbc25774249c0
Rommel Rico <me@rommelrico.com>
2022-06-19 14:11:53 -0700

    :sparkles: Cohesion: Increasing Payment Processor Cohesion 2

--- 02_TheSevenPrinciplesOfModernSoftwareDesign/02_HighCohesion/03_IncreasingPaymentProcessorCohesion_example.py
+++ 02_TheSevenPrinciplesOfModernSoftwareDesign/02_HighCohesion/03_IncreasingPaymentProcessorCohesion_example.py
@@ -1,4 +1,11 @@
+from enum import Enum, auto
 from typing import List
+
+
+class PaymentStatus(Enum):
+    """Payment status"""
+    OPEN = auto()
+    PAID = auto()
 
 
 class Order:
@@ -6,30 +13,28 @@ class Order:
         self.items: List[str] = []
         self.quantities: List[int] = []
         self.prices: List[int] = []
-        self.status: str = "open"
+        self.status: PaymentStatus = PaymentStatus.OPEN
 
     def add_item(self, name: str, quantity: int, price: int) -> None:
         self.items.append(name)
         self.quantities.append(quantity)
         self.prices.append(price)
 
-    def set_status(self, status: str) -> None:
+    def set_status(self, status: PaymentStatus) -> None:
         self.status = status
 
 
 class PaymentProcessor:
 
-    def pay(self, order: Order, payment_type: str, security_code: str) -> None:
-        if payment_type == "debit":
-            print("Processing debit payment type")
-            print(f"Verifying security code: {security_code}")
-            order.set_status("paid")
-        elif payment_type == "credit":
-            print("Processing credit payment type")
-            print(f"Verifying security code: {security_code}")
-            order.set_status("paid")
-        else:
-            raise ValueError(f"Unknown payment type: {payment_type}")
+    def pay_debit(self, order: Order, security_code: str) -> None:
+        print("Processing debit payment type")
+        print(f"Verifying security code: {security_code}")
+        order.set_status(PaymentStatus.PAID)
+
+    def pay_credit(self, order: Order, security_code: str) -> None:
+        print("Processing credit payment type")
+        print(f"Verifying security code: {security_code}")
+        order.set_status(PaymentStatus.PAID)
 
 
 def main() -> None:
@@ -39,7 +44,7 @@ def main() -> None:
     order.add_item("USB cable", 2, 500)
 
     processor = PaymentProcessor()
-    processor.pay(order, "debit", "0372846")
+    processor.pay_debit(order, "0372846")
 
 
 if __name__ == "__main__":
