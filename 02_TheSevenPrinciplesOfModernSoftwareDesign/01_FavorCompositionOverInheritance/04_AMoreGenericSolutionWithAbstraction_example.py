58bfd102adf0bdbc6282b179e38bc75e0bcb287e
Rommel Rico <me@rommelrico.com>
2022-06-18 22:40:48 -0700

    :sparkles: Composition: A More Generic Solution With Abstraction 2

--- 02_TheSevenPrinciplesOfModernSoftwareDesign/01_FavorCompositionOverInheritance/04_AMoreGenericSolutionWithAbstraction_example.py
+++ 02_TheSevenPrinciplesOfModernSoftwareDesign/01_FavorCompositionOverInheritance/04_AMoreGenericSolutionWithAbstraction_example.py
@@ -1,8 +1,33 @@
-from dataclasses import dataclass
-from typing import Optional
+from dataclasses import dataclass, field
+from typing import Protocol, List
 
 
-@dataclass
+class PaymentSource(Protocol):
+
+    def compute_pay(self) -> int:
+        ...
+
+
+@dataclass()
+class HourlyContract:
+    hourly_rate: int
+    hours_worked: float
+    employer_cost: int = 100000
+
+    def compute_pay(self):
+        return int(self.hourly_rate * self.hours_worked + self.employer_cost)
+
+
+@dataclass()
+class SalariedContract:
+    monthly_salary: int
+    percentage: float = 1
+
+    def compute_pay(self):
+        return int(self.monthly_salary * self.percentage)
+
+
+@dataclass()
 class DealBasedCommission:
     commission: int = 10000
     deals_landed: float = 0
@@ -11,58 +36,26 @@ class DealBasedCommission:
         return int(self.commission * self.deals_landed)
 
 
-@dataclass
-class HourlyEmployee:
+@dataclass()
+class Employee:
     name: str
     id: int
-    pay_rate: int = 0
-    hours_worked: float = 0
-    employer_cost: int = 100000
-    commission: Optional[DealBasedCommission] = None
+    payment_sources: List[PaymentSource] = field(default_factory=list)
+
+    def add_payment_source(self, payment_source: PaymentSource):
+        self.payment_sources.append(payment_source)
 
     def compute_pay(self) -> int:
-        total = int(self.pay_rate * self.hours_worked + self.employer_cost)
-        if self.commission:
-            total += self.commission.compute_pay()
-        return total
-
-
-@dataclass
-class SalariedEmployee:
-    name: str
-    id: int
-    monthly_salary: int = 0
-    percentage: float = 1
-    commission: Optional[DealBasedCommission] = None
-
-    def compute_pay(self) -> int:
-        total = int(self.monthly_salary * self.percentage)
-        if self.commission:
-            total += self.commission.compute_pay()
-        return total
-
-
-@dataclass
-class Freelancer:
-    name: str
-    id: int
-    pay_rate: int = 0
-    hours_worked: float = 0
-    vat_number: str = ""
-    commission: Optional[DealBasedCommission] = None
-
-    def compute_pay(self) -> int:
-        total = int(self.pay_rate * self.hours_worked)
-        if self.commission:
-            total += self.commission.compute_pay()
-        return total
+        return sum(source.compute_pay() for source in self.payment_sources)
 
 
 def main():
-    henry = HourlyEmployee(name="Henry", id=123456, pay_rate=5000, hours_worked=100)
+    henry = Employee(name="Henry", id=123456)
+    henry.add_payment_source(HourlyContract(hourly_rate=5000, hours_worked=100))
     print(f"{henry.name} earned ${(henry.compute_pay() / 100):.2f}.")
-    sarah_commission = DealBasedCommission(deals_landed=10)
-    sarah = SalariedEmployee(name="Sarah", id=47832, monthly_salary=500000, commission=sarah_commission)
+    sarah = Employee(name="Sarah", id=47832)
+    sarah.add_payment_source(SalariedContract(monthly_salary=500000))
+    sarah.add_payment_source(DealBasedCommission(deals_landed=10))
     print(f"{sarah.name} earned ${(sarah.compute_pay() / 100):.2f}.")
 
 
