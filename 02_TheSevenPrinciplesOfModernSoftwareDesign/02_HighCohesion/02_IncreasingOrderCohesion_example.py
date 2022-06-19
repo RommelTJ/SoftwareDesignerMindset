160729360f19e409f6d5fce9444c36b2b3f3a29d
Rommel Rico <me@rommelrico.com>
2022-06-19 14:05:50 -0700

    :sparkles: Cohesion: Increasing Order Cohesion 2

--- 02_TheSevenPrinciplesOfModernSoftwareDesign/02_HighCohesion/02_IncreasingOrderCohesion_example.py
+++ 02_TheSevenPrinciplesOfModernSoftwareDesign/02_HighCohesion/02_IncreasingOrderCohesion_example.py
@@ -13,15 +13,21 @@ class Order:
         self.quantities.append(quantity)
         self.prices.append(price)
 
-    def pay(self, payment_type: str, security_code: str) -> None:
+    def set_status(self, status: str) -> None:
+        self.status = status
+
+
+class PaymentProcessor:
+
+    def pay(self, order: Order, payment_type: str, security_code: str) -> None:
         if payment_type == "debit":
             print("Processing debit payment type")
             print(f"Verifying security code: {security_code}")
-            self.status = "paid"
+            order.set_status("paid")
         elif payment_type == "credit":
             print("Processing credit payment type")
             print(f"Verifying security code: {security_code}")
-            self.status = "paid"
+            order.set_status("paid")
         else:
             raise ValueError(f"Unknown payment type: {payment_type}")
 
@@ -31,7 +37,9 @@ def main() -> None:
     order.add_item("Keyboard", 1, 5000)
     order.add_item("SSD", 1, 15000)
     order.add_item("USB cable", 2, 500)
-    order.pay("debit", "0372846")
+
+    processor = PaymentProcessor()
+    processor.pay(order, "debit", "0372846")
 
 
 if __name__ == "__main__":
