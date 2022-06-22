42e76b0cd7b8d986eccecc17a6873dd2e8051390
Rommel Rico <me@rommelrico.com>
2022-06-22 07:57:26 -0700

    :tada: Abstractions: Order and LineItem classes

---
+++ 02_TheSevenPrinciplesOfModernSoftwareDesign/05_DependOnAbstractions/example1/pos/order.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from pos.data import PaymentStatus
+
+
+@dataclass()
+class LineItem:
+    item: str
+    quantity: int
+    price: int
+
+    @property
+    def total_price(self) -> int:
+        return self.quantity * self.price
+
+
+@dataclass()
+class Order:
+    items: List[LineItem] = field(default_factory=list)
+    status: PaymentStatus = PaymentStatus.OPEN
+
+    def add_item(self, item: LineItem) -> None:
+        self.items.append(item)
+
+    @property
+    def total_price(self) -> int:
+        return sum(item.total_price for item in self.items)
