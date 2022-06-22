78fac712831b86e67ab48e0efa22057632151bc9
Rommel Rico <me@rommelrico.com>
2022-06-22 07:58:02 -0700

    :tada: Abstraction: Main example

---
+++ 02_TheSevenPrinciplesOfModernSoftwareDesign/05_DependOnAbstractions/example1/main.py
@@ -0,0 +1,17 @@
+from pos.order import LineItem, Order
+from pos.payment import PaymentProcessor
+
+
+def main():
+    order = Order()
+    order.add_item(LineItem("Keyboard", 1, 5000))
+    order.add_item(LineItem("SSD", 1, 15000))
+    order.add_item(LineItem("USB Cable", 2, 500))
+
+    print(f"The total price is: ${(order.total_price / 100):.2f}.")
+    processor = PaymentProcessor("sms")
+    processor.pay_credit(order, "123456")
+
+
+if __name__ == "__main__":
+    main()
