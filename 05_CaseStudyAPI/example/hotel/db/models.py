902214534c227dffe56346ef160ba6cdb770a732
Rommel Rico <me@rommelrico.com>
2022-07-23 12:15:50 -0700

    :sparkles: Scaffolding: Setting up the Database models

---
+++ 05_CaseStudyAPI/example/hotel/db/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy.sql.sqltypes import Integer, String, Date
+
+Base = declarative_base()
+
+
+class DBCustomer(Base):
+    __tablename__ = "customer"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email_address = Column(String(250), nullable=False)
+
+
+class DBRoom(Base):
+    __tablename__ = "room"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    number = Column(String(250), nullable=False)
+    size = Column(Integer, nullable=False)
+    price = Column(Integer, nullable=False)
+
+
+class DBBooking(Base):
+    __tablename__ = "booking"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    from_date = Column(Date, nullable=False)
+    to_date = Column(Date, nullable=False)
+    customer: DBCustomer
+    room: DBRoom
+    price = Column(Integer, nullable=False)
